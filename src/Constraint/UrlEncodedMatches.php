f3a7813e704b558b39a858990e91d8af43104d2b
Martin Helmich <m.helmich@mittwald.de>
2018-04-04 23:43:02 +0200

    Make it work

---
+++ src/Constraint/UrlEncodedMatches.php
@@ -0,0 +1,56 @@
+<?php
+namespace Helmich\Psr7Assert\Constraint;
+
+use PHPUnit\Framework\Constraint\Constraint;
+use PHPUnit\Framework\Constraint\IsAnything;
+use PHPUnit\Framework\Constraint\IsEqual;
+
+class UrlEncodedMatches extends Constraint
+{
+    private $nameMatcher;
+    private $valueMatcher;
+
+    public function __construct($nameMatcher, $valueMatcher = null)
+    {
+        parent::__construct();
+
+        if (!($nameMatcher instanceof Constraint)) {
+            $nameMatcher = new IsEqual($nameMatcher);
+        }
+
+        if ($valueMatcher === null) {
+            $valueMatcher = new IsAnything();
+        } else if (!($valueMatcher instanceof Constraint)) {
+            $valueMatcher = new IsEqual($valueMatcher);
+        }
+
+        $this->nameMatcher = $nameMatcher;
+        $this->valueMatcher = $valueMatcher;
+    }
+
+    protected function matches($other): bool
+    {
+        parse_str($other, $parsedQuery);
+
+        foreach ($parsedQuery as $key => $value) {
+            if (!$this->nameMatcher->evaluate($key, "", true)) {
+                continue;
+            }
+
+            if (!$this->valueMatcher->evaluate($value, "", true)) {
+                continue;
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public function toString(): string
+    {
+        return 'contains a name matching ' . $this->nameMatcher->toString() . ' and value matching ' . $this->valueMatcher->toString();
+    }
+
+
+}
